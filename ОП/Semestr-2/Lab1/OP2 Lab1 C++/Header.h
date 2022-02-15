bd93b292e65071b0b67c97903d60c89a04e90473
Andrey7904 <88614361+Andrey7904@users.noreply.github.com>
2022-02-15 11:45:19 +0200

    Add files via upload

---
+++ ОП/Semestr-2/Lab1/OP2 Lab1 C++/Header.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <Windows.h>
+using namespace std;
+
+void print(string s);
+void editing(ifstream& inFile);
